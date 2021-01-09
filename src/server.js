edb421e4d44d3398a54bc22cb498a41b364d2240
Corentin Bacconnais <corentin.bacconnais@gmail.com>
2021-01-09 03:34:46 +0100

    Try to add https

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const app = require('./app')
-const port = 1905
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+var path = require('path')
 
-app.listen(port, () => {
-  console.log(`App listening in ${port}`)
-})
+const key = fs.readFileSync(path.resolve('./src/key.pem'))
+const cert = fs.readFileSync(path.resolve('./src/cert.pem'))
+
+http.createServer(app).listen(1905)
+https.createServer({key: key, cert: cert }, app).listen(1906)
